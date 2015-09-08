793ce1d2f4a840d26d851fedddcd7acfaa179397
Mark Arts <markarts@hoppinger.com>
2015-09-08 14:28:50 +0200

    [WIP] refactord the update method from the character2d

--- Assets/scripts/PlatformerCharacter2D.cs
+++ Assets/scripts/PlatformerCharacter2D.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 namespace UnityStandardAssets._2D
 {
@@ -8,17 +9,22 @@ namespace UnityStandardAssets._2D
         [SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
         [SerializeField] private float m_JumpForce = 400f;                  // Amount of force added when the player jumps.
         [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
-        [SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
+    //    [SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
         [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
 
         private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
-        const float k_GroundedRadius = .01f; // Radius of the overlap circle to determine if grounded
+        const float k_GroundedRadius = .05f; // Radius of the overlap circle to determine if grounded
         private bool m_Grounded;            // Whether or not the player is grounded.
         private Transform m_CeilingCheck;   // A position marking where to check for ceilings
         const float k_CeilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
         private Animator m_Anim;            // Reference to the player's animator component.
         private Rigidbody2D m_Rigidbody2D;
         private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+
+		//input
+		private bool jump = false;
+		private bool crouch = false;
+		private float speedX = 0f;
 
         private void Awake()
         {
@@ -29,80 +35,82 @@ namespace UnityStandardAssets._2D
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
         }
 
-        private void FixedUpdate()
-        {
+		void Update(){
+			// set the rotation of the player according to the current gravity
 			transform.rotation = Quaternion.AngleAxis (Gravity.angle(), new Vector3 (0, 0, 1));
 
-            m_Grounded = false;
+			// check if we are touching the ground
+			m_Grounded = isGrounded();
+			m_Anim.SetBool("Ground", m_Grounded);
 
-            // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-            // This can be done using layers instead but Sample Assets will not overwrite your project settings.
-            Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
-            for (int i = 0; i < colliders.Length; i++)
-            {
-                if (colliders[i].gameObject != gameObject)
-                    m_Grounded = true;
-            }
+			// Set the vertical animation
+			m_Anim.SetFloat("vSpeed", Gravity.gravitize(m_Rigidbody2D.velocity).y);
 
-            m_Anim.SetBool("Ground", m_Grounded);
+			input ();
 
-            // Set the vertical animation
-            m_Anim.SetFloat("vSpeed", Gravity.gravitize(m_Rigidbody2D.velocity).y);
-        }
+			// If crouching and trying to stand up, check to see if the character can stand up
+			if (!crouch && m_Anim.GetBool("Crouch") && !canStand())
+			{
+				crouch = true;
+			}
+			// Set whether or not the character is crouching in the animator
+			m_Anim.SetBool("Crouch", crouch);
+
+			// Reduce the speed if crouching by the crouchSpeed multiplier
+			speedX = (crouch ? speedX*m_CrouchSpeed : speedX);
+		
+			//only control the player if grounded or airControl is turned on
+			if (m_Grounded)
+			{				
+				// The Speed animator parameter is set to the absolute value of the horizontal input.
+				m_Anim.SetFloat("Speed", Mathf.Abs(speedX));
+				
+				// Move the character
+				m_Rigidbody2D.velocity = Gravity.gravitize(new Vector2((speedX*m_MaxSpeed)*Time.deltaTime, m_Rigidbody2D.velocity.y));
+				
+				// if the moving direction of the character changes
+				if ( (speedX > 0 && !m_FacingRight) || (speedX < 0 && m_FacingRight) )
+				{
+					Flip();
+				}
+			}
+
+			if (m_Grounded && jump)
+			{
+				// Add a vertical force to the player.
+				m_Grounded = false;
+				m_Anim.SetBool("Ground", false);
+				m_Rigidbody2D.AddForce(Gravity.gravitize(new Vector2(0f, m_JumpForce)));
+			}
 
 
-        public void Move(float move, bool crouch, bool jump)
-        {
+			jump = false;
+		}
 
-            // If crouching, check to see if the character can stand up
-            if (!crouch && m_Anim.GetBool("Crouch"))
-            {
-                // If the character has a ceiling preventing them from standing up, keep them crouching
-                if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
-                {
-                    crouch = true;
-                }
-            }
+		private bool canStand(){
+			return Physics2D.OverlapCircle (m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround);
+		}
 
-            // Set whether or not the character is crouching in the animator
-            m_Anim.SetBool("Crouch", crouch);
+		private void input(){
+			if (!jump && m_Grounded)
+			{
+				// Read the jump input in Update so button presses aren't missed.
+				jump = CrossPlatformInputManager.GetButtonDown("Jump");
+			}
 
-            //only control the player if grounded or airControl is turned on
-            if (m_Grounded)
-            {
-                // Reduce the speed if crouching by the crouchSpeed multiplier
-                move = (crouch ? move*m_CrouchSpeed : move);
+			crouch = Input.GetKey(KeyCode.LeftControl);
+			speedX = CrossPlatformInputManager.GetAxis("Horizontal");
+		}
 
-                // The Speed animator parameter is set to the absolute value of the horizontal input.
-                m_Anim.SetFloat("Speed", Mathf.Abs(move));
+		private bool isGrounded(){
+			Collider2D[] colliders = Physics2D.OverlapCircleAll (m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+			for (int i = 0; i < colliders.Length; i++) {
+				if (colliders [i].gameObject != gameObject)
+					return true;
+			}
 
-                // Move the character
-                m_Rigidbody2D.velocity = Gravity.gravitize(new Vector2(move*m_MaxSpeed, m_Rigidbody2D.velocity.y));
-
-                // If the input is moving the player right and the player is facing left...
-                if (move > 0 && !m_FacingRight)
-                {
-                    // ... flip the player.
-                    Flip();
-                }
-                    // Otherwise if the input is moving the player left and the player is facing right...
-                else if (move < 0 && m_FacingRight)
-                {
-                    // ... flip the player.
-                    Flip();
-                } 
-            }
-
-            // If the player should jump...
-            if (m_Grounded && jump && m_Anim.GetBool("Ground"))
-            {
-                // Add a vertical force to the player.
-                m_Grounded = false;
-                m_Anim.SetBool("Ground", false);
-                m_Rigidbody2D.AddForce(Gravity.gravitize(new Vector2(0f, m_JumpForce)));
-            }
-        }
-
+			return false;
+		}
 
         private void Flip()
         {
