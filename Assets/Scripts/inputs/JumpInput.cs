f6e34a43d67ca16c9d5e2da36cb85270b52e0a54
Mark Arts <markarts@hoppinger.com>
2015-09-10 15:18:50 +0200

    refactored desktop is jumping code

--- Assets/Scripts/inputs/JumpInput.cs
+++ Assets/Scripts/inputs/JumpInput.cs
@@ -9,11 +9,7 @@ namespace UnityStandardAssets._2D
 			#if UNITY_IPHONE || UNITY_ANDROID
 				cha.jump = Input.touchCount > 0 && cha.m_Grounded;
 			#else
-				if (!cha.jump && cha.m_Grounded)
-				{
-					// Read the jump input in Update so button presses aren't missed.
-					cha.jump = CrossPlatformInputManager.GetButtonDown("Jump");
-				}
+				cha.jump = !cha.jump && cha.m_Grounded && CrossPlatformInputManager.GetButtonDown("Jump");
 			#endif
 		}
 	}
