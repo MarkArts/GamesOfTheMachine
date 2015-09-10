edff730cbf426623bf0a4a0d748af9c36ca6dbfc
Mark Arts <markarts@hoppinger.com>
2015-09-10 15:20:02 +0200

    added extra can jump check for phones

--- Assets/Scripts/inputs/JumpInput.cs
+++ Assets/Scripts/inputs/JumpInput.cs
@@ -7,7 +7,7 @@ namespace UnityStandardAssets._2D
 	public class JumpInput : InputBehaviour {
 		public void input(PlatformerCharacter2D cha){
 			#if UNITY_IPHONE || UNITY_ANDROID
-				cha.jump = Input.touchCount > 0 && cha.m_Grounded;
+				cha.jump = !cha.jump && cha.m_Grounded && Input.touchCount > 0;
 			#else
 				cha.jump = !cha.jump && cha.m_Grounded && CrossPlatformInputManager.GetButtonDown("Jump");
 			#endif
