16d7a46c7410d2102256b7cc74d1667013650593
MarkArts <markarts@hoppinger.com>
2015-09-18 11:07:24 +0200

    made the tilt more sensitive

--- Assets/Scripts/inputs/WalkControlInput.cs
+++ Assets/Scripts/inputs/WalkControlInput.cs
@@ -8,9 +8,9 @@ public class WalkControlInput : InputBehaviour {
 		#if UNITY_IPHONE || UNITY_ANDROID
 		float angle = Gravity.tiltAngle();
 
-        if (angle > 0.1f)
+        if (angle > 0.05f)
             cha.speedX = 1f;
-        else if (angle < -0.1f)
+        else if (angle < -0.05f)
             cha.speedX = -1f;
         else
             cha.speedX = 0f;
