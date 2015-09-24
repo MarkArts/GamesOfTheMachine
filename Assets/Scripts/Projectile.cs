b02ed4b7be35d102aa844af67cdfc764e04a277c
MarkArts <markarts@hoppinger.com>
2015-09-24 12:15:20 +0200

    added lasor turret

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+    public float speedX;
+    public float speedY;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag != "Player" && other.tag != "Turret")
+        {
+            Die();
+        }
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(speedX, speedY, 0f) * Time.deltaTime;
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+
+    public GameObject projectile;
+
+    public float bulletSpeed;
+
+    public float fireRate;
+    public float fireOfset;
+    private Vector2 bulletVelocity;
+
+    private Transform spawn;
+    private Transform target;
+
+	// Use this for initialization
+	void Start () {
+        spawn = transform.Find("spawn");
+        target = transform.Find("target");
+
+        bulletVelocity = (target.position - spawn.position).normalized * bulletSpeed;
+
+        InvokeRepeating("Shoot", fireOfset, fireRate);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Shoot()
+    {
+        GameObject proj = (GameObject)Instantiate(projectile, spawn.position, Quaternion.identity);
+        proj.GetComponent<Projectile>().speedX = bulletVelocity.x;
+        proj.GetComponent<Projectile>().speedY = bulletVelocity.y;
+    }
+
+    void OnDrawGizmos()
+    {
+        if(transform.Find("spawn") && transform.Find("target"))
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(transform.Find("spawn").position, transform.Find("target").position);
+        }
+    }
+}
