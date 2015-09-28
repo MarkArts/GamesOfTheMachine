96790699bec3ab39889fe39d5dcc15f655737c8c
Mark Arts <markarts@hoppinger.com>
2015-09-28 16:45:03 +0200

    fixed not being able to stand still

--- Assets/Scripts/inputs/WalkControlInput.cs
+++ Assets/Scripts/inputs/WalkControlInput.cs
@@ -8,10 +8,10 @@ public class WalkControlInput : InputBehaviour {
 		#if UNITY_IPHONE || UNITY_ANDROID
 		float angle = Gravity.tiltAngle();
 
-        if (angle < 0.05f)
-            cha.speedX = 1f;
-        else if (angle > -0.05f)
+        if (angle > 0.15f)
             cha.speedX = -1f;
+        else if (angle < -0.15f)
+            cha.speedX = 1f;
         else
             cha.speedX = 0f;
 		#else
