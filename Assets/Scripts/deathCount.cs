a05e8338e9ca02d97d302f5383d1d0e47c1f199e
Mark Arts <markarts@hoppinger.com>
2015-10-05 13:18:24 +0200

    added death counter

---
+++ Assets/Scripts/deathCount.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class deathCount : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<TextMesh>().text = PlayerPrefs.GetInt("deaths", 0).ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
