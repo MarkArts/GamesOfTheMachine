64f8a900adec18b292e7a4437b3a154ef1cd26f4
Mark Arts <markarts@hoppinger.com>
2015-10-05 16:55:56 +0200

    added blade and saw traps

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(new Vector3(0f, 0f, speed));
+	}
+}
